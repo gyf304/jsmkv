8f028feacf8021b3fb78d6e44ff9da8985b0415b
Yifan Gu <me@yifangu.com>
2024-06-09 22:35:01 -0400

    fix demo

--- demo/build.ts
+++ demo/build.ts
@@ -3,7 +3,8 @@ import { $ } from "bun";
 await $`mkdir -p demo/dist`;
 await $`bun build --minify --sourcemap src/index.ts > demo/dist/index.js`;
 await $`cp demo/src/*.html demo/dist/`;
-if (!Bun.file("demo/dist/spring-blender-open-movie.mkv").exists()) {
+const demoFileExists = await Bun.file("demo/dist/spring-blender-open-movie.mkv").exists();
+if (!demoFileExists) {
 	await $`curl -Lo demo/dist/spring-blender-open-movie.mkv https://public-files.yifangu.com/videos/spring-blender-open-movie.mkv`;
 }
 
